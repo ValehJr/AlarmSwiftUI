867ba473a867d0c3bdce31fad15abb8eca9bce67
Valeh <i.valehjr@gmail.com>
2024-09-21 21:35:50 +0400

    Clock and Alarm functions final setup

---
+++ AlarmSwiftUI/View/AlarmEditView.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmEditView.swift
+//  AlarmSwiftUI
+//
+//  Created by Valeh Ismayilov on 19.09.24.
+//
+
+import SwiftUI
+
+struct AlarmEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlarmEditView()
+}
